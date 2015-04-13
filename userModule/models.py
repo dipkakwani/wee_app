705f119adc62429c8a5e36abbc6e9f720b699504
dipkakwnai <dipkakwani@gmail.com>
2015-04-13 23:57:17 +0530

    add groupModule and other user models

--- userModule/models.py
+++ userModule/models.py
@@ -1,14 +1,21 @@
+"""
+Author : Diptanshu Kakwani
+Contains all the models that are realted to users.
+"""
 from django.db import models
 import os
 
+#Returns the path of the profile picture
 def getImagePath(instance, filename):
     return os.path.join('static/userModule/images', str(instance.userid), filename)
     
+#Returns the path of the default profile picture
 def defaultImage():
     return os.path.join('static/userModule/images', 'default.png')
 
+#Model storing the information of all the registered users.
 class User(models.Model):
-    userid = models.AutoField(primary_key=True)
+    userId = models.AutoField(primary_key=True)
 
     name = models.CharField(max_length=30)
 
@@ -40,3 +47,25 @@ class User(models.Model):
 
 #TODO: Add all user related tables.
 #NOTE:Django doesn't support composite primary key, so create a single compound unique key with Meta.unique_together .
+
+"""
+A many-to-many relationship on User.
+The order in which the user id appears determines which user has sent the friend request. 
+This approach avoids storing two tuples per friendship.
+"""
+class Friendship(models.Model):
+    userA = models.ForeignKey('User', related_name='Friendship.userA')
+    userB = models.ForeignKey('User', related_name='Friendship.userB')
+    STATUS = (('R', 'RequestSent'), ('F', 'Friends'),)
+    status = models.CharField(max_length=1, choices=STATUS)
+    class Meta:
+        unique_together = (("userA"), ("userB"),)
+
+#A many-to-many relationship on User.
+class Following(models.Model):
+    userA = models.ForeignKey('User', related_name='Following.userA')
+    userB = models.ForeignKey('User', related_name='Following.userB')
+    STATUS = (('F', 'Following'),)
+    status = models.CharField(max_length=1, choices=STATUS)
+    class Meta:
+        unique_together = (("userA"), ("userB"),)
