2610118149de6982fdcb0090abaf2b3c7d2afdfd
dipkakwnai <dipkakwani@gmail.com>
2015-04-14 12:06:01 +0530

    Bug fixes

--- groupModule/admin.py
+++ groupModule/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from groupModule.models import Group
+from groupModule.models import Joined
+from groupModule.models import Post
 
 # Register your models here.
+
 admin.site.register(Group)
-admin.site.registe(Joined)
+admin.site.register(Joined)
 admin.site.register(Post)
--- userModule/admin.py
+++ userModule/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from userModule.models import User
+from userModule.models import Friendship
+from userModule.models import Following
 # Register your models here.
 
 admin.site.register(User)
